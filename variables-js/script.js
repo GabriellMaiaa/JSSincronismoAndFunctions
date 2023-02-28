11ceb10377abe091416cf05d3fea0b70ec67371e
Gabriel Maia <gribeiromaia736@gmail.com>
2023-02-28 11:30:43 -0300

    variaveis

---
+++ variables-js/script.js
@@ -0,0 +1,14 @@
+//Var, Let e Const
+let altura = 5;
+let comprimento = 8;
+
+let mult = altura * comprimento
+console.log(mult)
+let forma= 'triangulo'
+
+if(forma === 'retangulo') {
+    area = altura * comprimento;
+} else {
+    area = (altura * comprimento) / 2
+}
+console.log(area)
