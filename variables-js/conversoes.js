dc1a60689f44df6872993b46820826e3ab696890
Gabriel Maia <gribeiromaia736@gmail.com>
2023-02-28 11:52:20 -0300

    conversoes

--- variables-js/conversoes.js
+++ variables-js/conversoes.js
@@ -3,3 +3,6 @@ const numero = 456;
 const numeroString= '456';
 
 console.log(numero == numeroString)
+console.log(numero + numeroString)// Desse jeito ele concatena
+
+console.log(numero + Number(numeroString))// Aqui ele converte para number
