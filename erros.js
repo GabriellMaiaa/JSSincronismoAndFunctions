ed0a5ee1d63b698757009e1e14bc6fbadc921472
Gabriel Maia <gribeiromaia736@gmail.com>
2023-03-01 11:15:22 -0300

    adicionando mais coisas

---
+++ erros.js
@@ -0,0 +1,8 @@
+const numero1 = 0;
+
+let numero;
+//tratamento de erros
+
+const idade = 16
+const erro = idade <=17 ? console.error('Você não pode beber') : console.log('Você pode!')
+
---
+++ variables-js/caracteristicas.js
@@ -0,0 +1,7 @@
+//O Javascript é uma linguagem de tipagem dinâmica
+//Não precisa declarar o tipo ao por uma variável
+
+let texto ='Olá'
+console.log(texto)
+texto = 35;
+console.log(texto)
