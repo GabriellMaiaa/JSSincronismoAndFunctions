ad6d44948a52d11256ebb91dceb4c353d7feee07
Gabriel Maia <gribeiromaia736@gmail.com>
2023-03-01 11:50:09 -0300

    functions

---
+++ functionsJs/funcoes.js
@@ -0,0 +1,11 @@
+let x =''
+
+function imprimeTexto (text) {
+    console.log(text)
+}
+imprimeTexto('slaaa')
+
+function soma() {// As funções devem ter um return sempre
+    return console.log(2 + 2)// Como não tem parâmetro, o return faz retornar mesmo assim.
+}
+soma();
