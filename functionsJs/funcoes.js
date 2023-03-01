0d5ef608b195bfbbee41c54e871a4366930d911a
Gabriel Maia <gribeiromaia736@gmail.com>
2023-03-01 12:05:57 -0300

    func

--- functionsJs/funcoes.js
+++ functionsJs/funcoes.js
@@ -5,7 +5,21 @@ function imprimeTexto (text) {
 }
 imprimeTexto('slaaa')
 
-function soma() {// As funções devem ter um return sempre
-    return console.log(2 + 2)// Como não tem parâmetro, o return faz retornar mesmo assim.
+function soma(x, y) {// As funções devem ter um return sempre
+    return x + y// Como não tem parâmetro, o return faz retornar mesmo assim.
 }
 soma();
+
+//Parâmetros de função
+function nomeIdade (nome, idade) {
+    return `meu nome é ${nome}, e minha idade é ${idade}`
+}
+
+console.log(nomeIdade('Gabriel', 19))
+console.log(nomeIdade( 19, 'Gabriel'))
+
+function multiplica(x, y) {
+    return x * y
+}
+console.log(multiplica(soma(4, 6), soma(9, 6))
+)
