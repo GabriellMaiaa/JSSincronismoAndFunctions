02e5dc4256e2851880bc3b0767d951e03b547666
Gabriel Maia <gribeiromaia736@gmail.com>
2023-03-09 10:07:24 -0300

    listas e methods

---
+++ arrays/lista-duas-dimensoes.js
@@ -0,0 +1,7 @@
+const alunos = ['João', 'Juliana', 'Ana', 'Caio'];
+const medias = [10, 9, 7.5, 8];
+
+const alunosEMedias = [alunos, medias];
+console.log(alunosEMedias[0][1])
+
+console.log(`Quero a aluna 3 da lista1 que é: ${alunosEMedias[0][2]}`)
