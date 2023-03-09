a1e091740f5cbba678d264172348c7eb5b9656eb
Gabriel Maia <gribeiromaia736@gmail.com>
2023-03-09 10:52:50 -0300

    for classico

--- arrays/procurandoAluno.js
+++ arrays/procurandoAluno.js
@@ -6,12 +6,12 @@ const alunosEMedias = [alunos, medias];
 function exibeNomeENota(aluno) {
     if(alunosEMedias[0].includes(aluno)) {
         console.log(`O aluno ${aluno} está cadastrado`);
-
-        const indice = alunosEMedias[0].indexOf(aluno);//Puxa qual é o INDICE do Aluno se for correto
+        const [alunos, medias] = alunosEMedias;//é bem mais legível criar uma VARIAVEL
+        
+        const indice = alunos.indexOf(aluno);//Puxa qual é o INDICE do Aluno se for correto
         console.log(`O indice do aluno é ${indice}`)
     } else {
-        console.log('O aluno não está cadastraado')
+        console.log('O aluno não está cadastrado')
     }
 }
-
-exibeNomeENota('João')
+exibeNomeENota('Gabriel')
---
+++ for/for.js
@@ -0,0 +1,10 @@
+const numeros = [100, 200, 300, 400, 500, 600]
+
+//Primeira expressão é executada uma única vez
+//  inicialização, condição, expressão final
+for (i = 0 ; i <= 5 ; i++ ) {
+    console.log('Olá')
+}
+for(i = 0; i <= numeros.length; i++) {
+    console.log(numeros[i])
+}
