21da9614a65da9dc34f1ceeb0cb4a2500cc01aca
Gabriel Maia <gribeiromaia736@gmail.com>
2023-03-15 15:16:39 -0300

    obj em obj

---
+++ objetos/alterar.js
@@ -0,0 +1,11 @@
+const pessoa = {
+    nome: 'Gabriel',
+    profissao: 'Engenheiro'
+};
+pessoa.fone = '92992795598'//Adicionando fone ao objeto
+
+console.log(pessoa)
+//Para deletar uma propriede
+
+delete pessoa.fone
+console.log(pessoa)
---
+++ objetos/tipos.js
@@ -0,0 +1,7 @@
+const cliente = {
+    nome:'João',
+    idade:23,
+    email:'joao@firma.com',
+    telefone: ['1194895495', '9299273733']
+}
+console.log(cliente.telefone[0])
